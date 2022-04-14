c50536983890c66c68a83f016b0e793dd927b1d8
d-meshchanchuk <d.meshchanchuk@gmail.com>
2022-04-14 15:04:54 +0300

    init project, add liquibase

---
+++ src/main/java/com/senla/scooterrentalapp/ScooteRentalAppApplication.java
@@ -0,0 +1,13 @@
+package com.senla.scooterrentalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScooteRentalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScooteRentalAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senla/scooterrentalapp/ScooteRentalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senla.scooterrentalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScooteRentalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
