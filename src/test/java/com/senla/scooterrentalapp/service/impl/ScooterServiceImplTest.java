bdfb960e845c370d2971531007974bac8552db97
Dmitry <d.meshchanchuk@gmail.com>
2022-05-11 20:59:51 +0300

    updated some tests

--- src/test/java/com/senla/scooterrentalapp/service/impl/ScooterServiceImplTest.java
+++ src/test/java/com/senla/scooterrentalapp/service/impl/ScooterServiceImplTest.java
@@ -3,8 +3,10 @@ package com.senla.scooterrentalapp.service.impl;
 import com.senla.scooterrentalapp.dto.scooter.ScooterDto;
 import com.senla.scooterrentalapp.entity.scooter.Scooter;
 import com.senla.scooterrentalapp.mapper.ScooterMapper;
+import com.senla.scooterrentalapp.mapper.ScooterMapperImpl;
 import com.senla.scooterrentalapp.repository.ScooterRepository;
 import com.senla.scooterrentalapp.service.ScooterService;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -17,33 +19,39 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ScooterServiceImplTest {
 
-//    ScooterRepository repository = Mockito.mock(ScooterRepository.class);
-//    ScooterService service = new ScooterServiceImpl(repository);
-//    ScooterDto scooterDto = new ScooterDto(1L, "test", 100);
-//    Scooter scooter = ScooterMapper.SCOOTER_MAPPER.toScooter(scooterDto);
-//    List<Scooter> scooters = new ArrayList<>();
-//
-//    @Test
-//    void save() {
-//        ScooterDto result = service.save(scooterDto);
-//        Mockito.verify(repository).save(ScooterMapper.SCOOTER_MAPPER.toScooter(scooterDto));
-//        assertEquals(result, scooterDto);
-//    }
-//
-//    @Test
-//    void findAll() {
-//        Mockito.when(repository.findAll()).thenReturn(scooters);
-//        List<ScooterDto> scooterDtoList = service.findAll();
-//        Mockito.verify(repository, Mockito.times(1)).findAll();
-//        assertEquals(scooters, scooterDtoList.stream().map(ScooterMapper.SCOOTER_MAPPER::toScooter).collect(Collectors.toList()));
-//    }
-//
-//    @Test
-//    void findById() {
-//        Long id = scooter.getId();
-//        Mockito.when(repository.findById(id)).thenReturn(Optional.of(scooter));
-//        ScooterDto result = service.findById(id);
-//        Mockito.verify(repository, Mockito.times(1)).findById(id);
-//        assertEquals(scooterDto, result);
-//    }
+    ScooterRepository repository = Mockito.mock(ScooterRepository.class);
+    ScooterMapper scooterMapper = new ScooterMapperImpl();
+    ScooterService service = new ScooterServiceImpl(repository, scooterMapper);
+    Scooter scooter = Scooter.builder()
+            .id(1L)
+            .model("test")
+            .power(100)
+            .build();
+
+    List<Scooter> scooters = new ArrayList<>();
+
+    @Test
+    void save() {
+        ScooterDto result = service.save(scooterMapper.fromScooter(scooter));
+        Mockito.verify(repository).save(scooter);
+        assertEquals(scooterMapper.toScooter(result), scooter);
+    }
+
+    @Test
+    void findAll() {
+        Mockito.when(repository.findAll()).thenReturn(scooters);
+        List<ScooterDto> scooterDtoList = service.findAll();
+        Mockito.verify(repository, Mockito.times(1)).findAll();
+        assertEquals(scooters, scooterDtoList.stream().map(scooterMapper::toScooter).collect(Collectors.toList()));
+    }
+
+    @SneakyThrows
+    @Test
+    void findById() {
+        Long id = scooter.getId();
+        Mockito.when(repository.findById(id)).thenReturn(Optional.of(scooter));
+        ScooterDto result = service.findById(id);
+        Mockito.verify(repository, Mockito.times(1)).findById(id);
+        assertEquals(scooterMapper.toScooter(result), scooter);
+    }
 }
