9e30cc6c86904af38691d3d0a2d135f85782affe
Dmitry <d.meshchanchuk@gmail.com>
2022-08-23 10:03:53 +0300

    some updates

--- src/main/java/com/senla/scooterrentalapp/service/impl/OrderServiceImpl.java
+++ src/main/java/com/senla/scooterrentalapp/service/impl/OrderServiceImpl.java
@@ -8,7 +8,6 @@ import com.senla.scooterrentalapp.mapper.OrderMapper;
 import com.senla.scooterrentalapp.repository.OrderRepository;
 import com.senla.scooterrentalapp.repository.UserRepository;
 import com.senla.scooterrentalapp.service.OrderService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
